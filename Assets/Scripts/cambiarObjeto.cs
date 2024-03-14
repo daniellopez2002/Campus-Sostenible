7a45af2765e44ea444797fd35cc58de2cbef91cd
EstebanBc03 <89149036+EstebanBc03@users.noreply.github.com>
2024-03-14 15:45:32 -0500

    Cambio de escenas
    Creacion de script con cambio de escenas para el inicio, login y registro

---
+++ Assets/Scripts/cambiarObjeto.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiarObjeto : MonoBehaviour
+{
+    public GameObject registroOn;
+    public GameObject registroOff;
+
+    public GameObject InicioOn;
+    public GameObject IncioOff;
+
+    // Método para mostrar un objeto y apagar otro
+    public void MostrarYApagar1()
+    {
+        // Verificar que ambos objetos estén asignados
+        if (registroOn != null && registroOff != null)
+        {
+            // Mostrar el objeto que queremos mostrar
+            registroOn.SetActive(true);
+
+            // Apagar el objeto que queremos apagar
+            registroOff.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("¡Asegúrate de asignar los objetos en el inspector de Unity!");
+        }
+    }
+
+    public void MostrarYApagar2()
+    {
+        // Verificar que ambos objetos estén asignados
+        if (InicioOn != null && IncioOff != null)
+        {
+            // Mostrar el objeto que queremos mostrar
+            InicioOn.SetActive(true);
+
+            // Apagar el objeto que queremos apagar
+            IncioOff.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("¡Asegúrate de asignar los objetos en el inspector de Unity!");
+        }
+    }
+}
+
